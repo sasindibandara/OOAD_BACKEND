0ebd6051dfc0ec23dd943dc028de07c76bb7d1dc
Sasindi Bandara <161796034+sasindibandara@users.noreply.github.com>
2025-05-26 11:47:37 +0530

    Update CorsConfig.java

--- full_eventura_backend/src/main/java/com/example/eventura/config/CorsConfig.java
+++ full_eventura_backend/src/main/java/com/example/eventura/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to all API endpoints
+        registry.addMapping("/api/**") // Apply to all API endpoints
                 .allowedOrigins(
                         "https://eventura-beige.vercel.app"
                 ) // Allow your frontend origins
